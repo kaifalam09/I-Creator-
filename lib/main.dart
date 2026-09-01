392d97d12de9c1dd78c8552031e6641e62a0cbae
kaifalam09 <mrkaifalam0311@gmail.com>
2026-09-01 16:56:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ICreatorApp());
 }
+
 
 // ============================================================
 // USER SESSION
