5254250c6906055be60d5776870cc15c276f48e8
kaifalam09 <mrkaifalam0311@gmail.com>
2026-09-01 16:54:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
